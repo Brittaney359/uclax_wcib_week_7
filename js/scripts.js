a72567ae01873d88747abb2d2ffedbd644a46061
Brittaney359 <bscherer359@gmail.com>
2021-05-17 22:43:11 -0700

    Week 7

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
 jQuery(function() {
 
+let my_switch = false;
+
 	$('#my_button').click(function () {
-	});
+
+	if ( my_switch == false ) {
+		my_switch = true;
+
+		$(this).html('Click for Puppy Girl').removeClass('btn-primary').addClass('btn-success');
+
+		$('#my_image').attr('src', 'images/puppy_boy.jpeg');
+	} else {
+		my_switch = false;
+
+		$(this).html('Click for Puppy Boy').removeClass('btn-primary').addClass('btn-primary');
+
+		$('#my_image').attr('src', 'images/puppy_girl.jpeg');
+	}
+
+	// if ( $('.square.alt').hasClass('red') ) {
+	// 	$('.square.alt').removeClass('red');
+	// } else {
+	// 	$('.square.alt').addClass('red');
+	// }
+
+
+});
 
 });
